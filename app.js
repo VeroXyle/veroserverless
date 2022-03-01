650151d6c4365b94f55ad3089f4c72ab9712a099
Neeraj Dalal <nd941z@gmail.com>
2022-03-01 19:02:25 +0530

    Tue Mar  1 19:02:25 IST 2022

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// initializing app
+const app = express();
+const PORT = 3003;
+
+// routes
+app.get('/', (req, res) => {
+  res.send(`Server is running successfully on ${PORT}`);
+});
+
+// running app
+app.listen(PORT, async () => {
+  console.log(`App is running on port - ${PORT}`);
+});
